a3428a0c0eeb9d591ee2d778448bb2149123f2a2
Ítalo Luz <zul.olati@gmail.com>
2023-06-11 19:31:46 -0300

    🐛 fix: Corrige bug no script do Bluetooth
    Foi corrigido um bug no script do Bluetooth que fazia com que o ícone do Bluetooth permanecesse visível mesmo quando o dispositivo estava desconectado. Agora, o ícone do Bluetooth é exibido corretamente quando um dispositivo está conectado e desaparece quando o Bluetooth é desconectado

--- .scripts/info/show-bluetooth.sh
+++ .scripts/info/show-bluetooth.sh
@@ -5,10 +5,12 @@ device_mac="$(bluetoothctl info | awk '/Device/ {print $2}')"
 
 if [ "${device_name}" != "" ]; then
     echo ""
+else
+    echo ""
 fi
 
 case "$1" in
 "full-info")
-        notify-send -a "Informações sobre o bluetooth" -r 6666 -i "$HOME/.icons/system/bluetooth.png" "Você está atualmente conectado a rede bluetooth \"${device_name}\" cujo o endereço MAC é \"${device_mac}\""
+    notify-send -a "Informações sobre o bluetooth" -r 6666 -i "$HOME/.icons/system/bluetooth.png" "Você está atualmente conectado a rede bluetooth \"${device_name}\" cujo o endereço MAC é \"${device_mac}\""
     ;;
 esac
